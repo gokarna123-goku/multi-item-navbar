0955aa6ed652b819b8730e60784b02170d996548
gokarna123-goku <agokarna444@gmail.com>
2023-05-29 11:01:30 +0545

    working on the making fixed navbar while scroll y

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ window.addEventListener("scroll", function () {
   var navbar = document.querySelector("nav");
   var scrollY = window.scrollY;
 
-  if (scrollY >= 20) {
+  if (scrollY >= 10) {
     navbar.classList.add("fixed");
   } else {
     navbar.classList.remove("fixed");
